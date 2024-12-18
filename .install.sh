05851c03fdfdbe3b52a734d8038940816bf8c7a8
Chris McCabe <chris.mccabe@neurovalens.com>
2024-12-18 09:33:30 +0000

    chore: Initial commit

---
+++ .install.sh
@@ -0,0 +1,43 @@
+## Taps
+echo "Tapping Brew..."
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+## Brew install
+echo "Installing Brew Formulae..."
+brew install lua
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew install borders
+brew install yabai
+brew install sketchybar
+
+## Install fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
+## Create symlinks
+rm -f ~/.config/yabai/yabairc
+mkdir -p ~/.config/yabai
+ln -s /Users/chris/Development/personal/dotfiles/.config/yabai/yabairc ~/.config/yabai/yabairc
+
+rm -f ~/.config/borders/bordersrc
+mkdir -p ~/.config/borders
+ln -s /Users/chris/Development/personal/dotfiles/.config/borders/bordersrc ~/.config/borders/bordersrc
+
+rm -f ~/.config/sketchybar
+ln -s /Users/chris/Development/personal/dotfiles/.config/sketchybar ~/.config/sketchybar
+
+## Start Services
+echo "Starting Services (grant permissions)..."
+brew services start borders
+brew services start sketchybar
+yabai --start-service
+
